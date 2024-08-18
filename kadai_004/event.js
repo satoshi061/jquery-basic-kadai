1576584b5f646648697cbda24a71a161528ec955
satoshi061 <shankou0302@gmail.com>
2024-08-18 19:44:04 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(window).on({
+    'load': () => {
+      console.log('loadイベントが発生しました');
+    },
+    'scroll': () => {
+      console.log('scrollイベントが発生しました');
+    },
+  });
+});
